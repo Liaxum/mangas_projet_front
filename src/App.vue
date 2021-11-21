ca47e2e899652992de8e3ad2c0512bbcc0a92dda
Romain <texier.r@gmail.com>
2021-11-21 17:03:03 +0100

    @add Adding the router system

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <ul>
+        <li class="nav-item"><router-link to="/">Home</router-link></li>
+        <li class="nav-item"><router-link to="/Test">Test</router-link></li>
+      </ul>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import HelloWorld from '@/views/HelloWorld'
+import Test from '@/views/Test'
+
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { 
+    path: '/',
+    component: HelloWorld
+  },
+  { 
+    path: '/Test',
+    component: Test
+  }
+];
+
+const  router = new VueRouter({ routes });
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
