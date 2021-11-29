55e513f027daff42015dab7b1e9034ec88651caf
Romain TEXIER <46653730+Liaxum@users.noreply.github.com>
2021-11-29 15:37:54 +0100

    @upd edit router and state

--- src/main.js
+++ src/main.js
@@ -1,28 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-
-import HelloWorld from '@/views/HelloWorld'
-import Test from '@/views/Test'
-
+import router from './router'
+import store from './state'
 
 Vue.config.productionTip = false
-Vue.use(VueRouter)
-
-const routes = [
-  { 
-    path: '/',
-    component: HelloWorld
-  },
-  { 
-    path: '/Test',
-    component: Test
-  }
-];
-
-const  router = new VueRouter({ routes });
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
