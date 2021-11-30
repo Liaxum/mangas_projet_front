81b842d8ffd5e94771bb8abdc5bdbdd1e93e0088
Romain TEXIER <46653730+Liaxum@users.noreply.github.com>
2021-11-30 08:54:22 +0000

    @add good practice

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/HelloWorld.vue')
+  }, 
+  {
+    path: '/Test',
+    name: 'Test',
+    component: () => import ('../views/Test.vue')
+  }
+]
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        <h1>Test</h1>
-    </div>
+  <div>
+    <h1> {{ test }} </h1>
+  </div>
 </template>
-
 <script>
-export default {};
+  export default {
+    name: 'Test',
+    created() {
+      /* eslint-disable no-console */
+      console.log(this.$t('test'))
+    },
+    data() {
+      return {
+        test: this.$t('test')
+      }
+    }
+  }
 </script>
-
-<style></style>
