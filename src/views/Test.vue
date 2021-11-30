eac9a7b8e415043b622789e3510327f2668bd55f
Romain <romain.texier@sourceamax.com>
2021-11-30 21:00:20 +0100

    @upd api test

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,19 +1,50 @@
 <template>
   <div>
-    <h1> {{ test }} </h1>
+    <h1> Liste des mangas </h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>createdAt</th>
+          <th>Price</th>
+          <th>description</th>
+          <th>stocks</th>
+          <th>Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in mangas" :key="item.id">
+          <td> {{item.nama}} </td>
+          <td> {{item.createdAt}} </td>
+          <td> {{item.price}} </td>
+          <td> {{item.description}} </td>
+          <td> {{item.stocks}} </td>
+          <td> {{item.image}} </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
-  export default {
-    name: 'Test',
-    created() {
-      /* eslint-disable no-console */
-      console.log(this.$t('test'))
-    },
-    data() {
-      return {
-        test: this.$t('test')
-      }
-    }
-  }
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Test',
+  created() {
+    this.fetchMangas();
+  },
+
+  data() {
+    return {
+      test: this.$t('test'),
+    };
+  },
+  computed: {
+    ...mapState('mangas', ['mangas']),
+  },
+  methods: {
+    ...mapActions('mangas', ['fetchMangas']),
+  },
+};
 </script>
