e70edeed6b22dbef9e66a820c079d83e41b59e05
Romain <romain.texier@sourceamax.com>
2021-12-09 18:03:23 +0100

    @add navbar

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,80 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
       dark
+      height="80"
     >
+      <v-img
+        src="./assets/logo.png"
+        max-height="70"
+        max-width="170"
+        contain
+      />
+      <v-tooltip bottom>
+        <template  v-slot:activator="{on, attrs}">
+          <v-btn to="/"
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-home</v-icon>
+          </v-btn>
+        </template>
+        <span class="text-caption">Home</span>
+      </v-tooltip>
 
+      <v-tooltip bottom>
+        <template  v-slot:activator="{on, attrs}">
+          <v-btn to="/Store"
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-store</v-icon>
+          </v-btn>
+        </template>
+        <span>Store</span>
+      </v-tooltip>
+      <v-spacer />
+      <v-tooltip bottom>
+        <template  v-slot:activator="{on, attrs}">
+          <v-btn to="/Cart"
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-cart</v-icon>
+          </v-btn>
+        </template>
+        <span>Cart</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template  v-slot:activator="{on, attrs}">
+          <v-btn to="/Account"
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+        <span>Account</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template  v-slot:activator="{on, attrs}">
+          <v-btn to="/logout"
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
+        </template>
+        <span>Disconnect</span>
+      </v-tooltip>
     </v-app-bar>
-
     <v-main>
-      <router-link to="/" >Home</router-link>
-      <router-link to="Test">Test</router-link>
-      <router-view/>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
